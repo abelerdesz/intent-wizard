b9ebc594d29d466a45dec1defef5ac000fb1a6ba
Ábel Erdész <abel.erdesz@gmail.com>
2023-02-02 08:36:58 +0100

    feat(font): change font to Inter

---
+++ src/theme/theme.ts
@@ -0,0 +1,22 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Inter',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"'
+    ].join(','),
+    h1: {
+      fontSize: 39.81
+    }
+  }
+});
